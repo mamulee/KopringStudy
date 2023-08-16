2405ddbd44db6124bf1995e7c10a37a85e75d0e6
mamulee <ghdah7@gmail.com>
2023-08-16 15:35:59 +0900

    getting started

---
+++ src/main/kotlin/com/example/kopring/Test.kt
@@ -0,0 +1,15 @@
+package com.example.kopring
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+fun main() {
+    GlobalScope.launch {
+        delay(1000L)
+        println("World!")
+    }
+
+    println("Hello,")
+    Thread.sleep(2000L)
+}
